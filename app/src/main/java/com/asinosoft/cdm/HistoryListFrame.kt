e9b0917f78240ac0a092fe0dd71162b040b60739
LinRiB <ismoilov.sharifdjon@gmail.com>
2020-06-01 11:29:45 +0300

    Небольшие исправления

--- app/src/main/java/com/asinosoft/cdm/HistoryListFrame.kt
+++ app/src/main/java/com/asinosoft/cdm/HistoryListFrame.kt
@@ -21,6 +21,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [HistoryListFrame.newInstance] factory method to
  * create an instance of this fragment.
  */
+
 class HistoryListFrame : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
