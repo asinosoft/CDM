b60d4d71d18938ffd4b51b1efc6f16c8f3b2eb47
evgen4m <shutikov.e@yandex.ru>
2020-06-27 11:40:10 +0200

    Добавлено отображение даты рождения

--- app/src/main/java/com/asinosoft/cdm/DetailContact/Contact.kt
+++ app/src/main/java/com/asinosoft/cdm/DetailContact/Contact.kt
@@ -4,6 +4,10 @@ import android.content.Context
 import android.database.Cursor
 import android.provider.ContactsContract
 import android.util.Log
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class Contact () {
@@ -15,12 +19,14 @@ class Contact () {
     val MIME_TYPE_VIBER_MSG = "vnd.android.cursor.item/vnd.com.viber.voip.viber_number_message"
     val MIME_TYPE_TELEGRAM = "vnd.android.cursor.item/vnd.org.telegram.messenger.android.profile"
     val MIME_TYPE_SKYPE = "vnd.android.cursor.item/com.skype4life.message"
+    val MIME_TYPE_BITHDAY = "vnd.android.cursor.item/contact_event"
 
     val TYPE_SKYPE = 121
     val TYPE_WHATSAPP = 122
     val TYPE_VIBER = 123
     val TYPE_EMAIL = 124
     val TYPE_TELEGRAM = 125
+    val TYPE_BITHDAY = 126
 
     var name: String? = null
     var contactID: String? = null
@@ -37,6 +43,9 @@ class Contact () {
     var mViberId = ArrayList<String>()
     var mTelegram = ArrayList<String>()
     var mTelegramId = ArrayList<String>()
+    var mBirthDay = ArrayList<String>()
+    var mBirthDayType = ArrayList<Int>()
+    var mAge = ArrayList<String>()
 
 
     fun parseDataCursor(id: String, context: Context){
@@ -82,12 +91,11 @@ class Contact () {
                  MIME_TYPE_VIBER_MSG -> getViberFromData(_id, data)
                  MIME_TYPE_TELEGRAM -> getTelegramFromData(_id, data)
                  MIME_TYPE_SKYPE -> getSkypeFromData(data)
+                 MIME_TYPE_BITHDAY -> getBithdayFromData(data, data2)
              }
              cursor.moveToNext()
         }
     }
-
-
 
     private fun getNumberFormData(data: String, data4: String, data2: String){
         if(null != data){
@@ -123,6 +131,20 @@ class Contact () {
         }
     }
 
+    private fun getBithdayFromData(data: String, data2: String){
+        if(null != data){
+            val stHelper = StHelper()
+            val date = stHelper.parseDateToddMMyyyy(data)
+            val date1 = stHelper.parseToMillis(data)
+            mBirthDay.add(date!!)
+            mAge.add(date1!!)
+            if(null != data2){
+                val type = Integer.parseInt(data2)
+                this.mBirthDayType.add(type)
+            }else this.mBirthDayType.add(-1)
+        }
+    }
+
     private fun getSkypeFromData(data: String?) {
         if (data != null) {
             this.mSkypeName = data
@@ -144,6 +166,7 @@ class Contact () {
             }
         }
     }
+
     private fun getWhatsAppVideoFromData(id: String, data: String){
         if((id != null) && (data != null)){
             val number: String = getNumberStr(data)
@@ -209,69 +232,59 @@ class Contact () {
         } else data
     }
 
+
+    
     fun getContactListForDetail(): ArrayList<ContactDetailListElement>{
 
         var result = ArrayList<ContactDetailListElement>()
+
         for (i in mPhoneNumbers.indices) {
             val numberStr: String = mPhoneNumbers[i]
             val element = ContactDetailListElement(0, numberStr, mPhoneTypes[i])
             result.add(element)
         }
 
-        if(mEmailAdress.size > 0){
-            for(i in mEmailAdress.indices){
-                val eMail: String = mEmailAdress[i]
-                val element = ContactDetailListElement(1, eMail, null, null,null, mEmailType[i])
-                result.add(element)
-            }
-        }
-
         if(mWhatsAppNumbers.size > 0){
             for(i in mWhatsAppNumbers.indices){
                 val numberStr = mWhatsAppNumbers[i]
-                val element = ContactDetailListElement(2, numberStr, TYPE_WHATSAPP, mWhatsAppCallId[i])
-                result.add(element)
-            }
-        }
-        if(mWhatsAppNumbers.size > 0){
-            for(i in mWhatsAppNumbers.indices){
-                val numberStr = mWhatsAppNumbers[i]
-                val element = ContactDetailListElement(3, numberStr, null, mWhatsAppCallId[i])
-                result.add(element)
-            }
-        }
-        if(mWhatsAppNumbers2.size > 0){
-            for(i in mWhatsAppNumbers2.indices){
-                val numberStr = mWhatsAppNumbers2[i]
-                val element = ContactDetailListElement(4, numberStr, null, mWhatsAppVideoId[i])
+                val element = ContactDetailListElement(1, numberStr, TYPE_WHATSAPP, mWhatsAppCallId[i], mWhatsAppVideoId[i], mPhoneNumbers[i])
                 result.add(element)
             }
         }
         if(mViberNumbers.size > 0){
             for(i in mViberNumbers.indices){
                 val numberStr = mViberNumbers[i]
-                val element = ContactDetailListElement(5, numberStr, TYPE_VIBER, mViberId[i])
-                val element1 = ContactDetailListElement(6, numberStr, null, mViberId[i])
-                result.add(element)
-                result.add(element1)
-            }
-        }
-        if(mTelegram.size > 0){
-            for(i in mTelegram.indices){
-                val numberStr = mPhoneNumbers[i]
-                val element = ContactDetailListElement(7, numberStr, TYPE_TELEGRAM, mTelegramId[i])
+                val element = ContactDetailListElement(2, numberStr, TYPE_VIBER, mViberId[i], null, mPhoneNumbers[i])
                 result.add(element)
             }
         }
-        if(null != mSkypeName){
+        if(mTelegramId.size > 0){
+            for(i in mTelegramId.indices){
+                val numberStr = mTelegramId[i]
+                val element = ContactDetailListElement(3, numberStr, TYPE_TELEGRAM, null, null, mPhoneNumbers[i])
+                result.add(element)
+            }
+        }
+        if(mSkypeName != null){
             val numberStr = mSkypeName
-            val element = ContactDetailListElement(8, numberStr, TYPE_SKYPE, contactID, name)
-            val element1 = ContactDetailListElement(9, numberStr, null, contactID, name)
+            val element = ContactDetailListElement(4, numberStr, TYPE_SKYPE, null, null, name)
             result.add(element)
-            result.add(element1)
-
+        }
+        if(mEmailAdress.size > 0){
+            for(i in mEmailAdress.indices){
+                val numberStr = mEmailAdress[i]
+                val element = ContactDetailListElement(5, numberStr, null, null, null, null, mEmailType[i])
+                result.add(element)
+            }
         }
 
+        if(mBirthDay.size > 0){
+            for(i in mBirthDay.indices){
+                val numberStr = mBirthDay[i]
+                val element = ContactDetailListElement(6, numberStr, TYPE_BITHDAY, mAge[i])
+                result.add(element)
+            }
+        }
         return result
     }
 }
