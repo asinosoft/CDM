863e155bfa7782d453aea79dcc6dd95d27643b6d
alef <alexeykrasilnikov76@gmail.com>
2021-12-06 21:12:47 +0700

    Добавить про используемые библиотеки #31

--- app/src/main/java/com/asinosoft/cdm/fragments/LicensesFragment.kt
+++ app/src/main/java/com/asinosoft/cdm/fragments/LicensesFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
 import androidx.fragment.app.Fragment
+import androidx.webkit.WebViewAssetLoader
 import com.asinosoft.cdm.R
+import com.asinosoft.cdm.helpers.LocalContentWebViewClient
 
 class LicensesFragment : Fragment() {
     override fun onCreateView(
@@ -13,6 +16,18 @@ class LicensesFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_licenses, container, false)
+        val view = inflater.inflate(R.layout.fragment_licenses, container, false)
+
+        val assetLoader = WebViewAssetLoader.Builder()
+            .addPathHandler("/assets/", WebViewAssetLoader.AssetsPathHandler(requireContext()))
+            .addPathHandler("/res/", WebViewAssetLoader.ResourcesPathHandler(requireContext()))
+            .build()
+
+        view.findViewById<WebView>(R.id.license_text).apply {
+            webViewClient = LocalContentWebViewClient(assetLoader)
+            loadUrl("https://appassets.androidplatform.net/assets/licenses.html")
+        }
+
+        return view
     }
 }
